941e8463a13e22d480e2db54726f81cb87328e00
Aaron Ortega <aortega@farmersfridge.com>
2021-12-26 19:26:07 -0600

    added example

--- convert_temperature/src/main.rs
+++ convert_temperature/src/main.rs
@@ -1,23 +1,28 @@
 // Temperature conversion between Celsius and Fahrenheit and vice versa.
 
+// EXAMPLE
+// Enter a unit to convert: F
+// Enter a temperature: 98
+// 98F is 36C
+
 use std::io;
 
 fn main() {
-    // let units = ["C", "F"];
     loop {
-        println!("Enter a unit to convert: ");
+        print!("Enter a unit to convert: ");
         let unit = read_input().trim().to_uppercase();
 
-        println!("Enter a temperature: ");
+        print!("Enter a temperature: ");
         let temperature: f64 = match read_input().trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
         if unit == "C" {
-            println!("{}{} is {}F", temperature, unit, c_to_f(temperature));
+            print!("{}{} is {}F", temperature, unit, c_to_f(temperature));
             break;
         } else if unit == "F" {
-            println!("{}{} is {}C", temperature, unit, f_to_c(temperature));
+            print!("{}{} is {}C", temperature, unit, f_to_c(temperature));
             break;
         } else {
             continue;
